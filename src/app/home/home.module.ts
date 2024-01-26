60346a542f5b6db767b9c9c35d40bb6ab4075954
SVLucas <lucassangurima2@gmail.com>
2024-01-26 09:46:46 -0500

    primer commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { PokeapiService } from '../Services/apiservice.service';
+import { AppComponent } from '../app.component';
 
 
 @NgModule({
@@ -12,8 +15,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+  HttpClientModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers:[PokeapiService],
+  bootstrap:[AppComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
+// Importa el servicio APIServiceService y el decorador Component desde los módulos de Angular.
+import { PokeapiService } from './../Services/apiservice.service';
 import { Component } from '@angular/core';
 
+// Define el componente HomePage con su selector, plantilla y estilos.
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  entrada1: string = "";
+  entrada2: string = "";
+  pokemon1: any;
+  pokemon2: any;
+  bounceClass: boolean = false;
 
-  constructor() {}
+  constructor(private api: PokeapiService) {}
 
+  getPokemonDataID() {
+    this.api.getPokemonID(+this.entrada1).subscribe((response) => {
+      this.pokemon1 = response;
+      console.log(this.pokemon1);
+      this.applyBounceAnimation();
+    });
+  }
+
+  getPokemonDataName() {
+    this.api.getPokemonName(this.entrada2.toLowerCase()).subscribe((response) => {
+      this.pokemon2 = response;
+      console.log(this.pokemon2);
+      this.applyBounceAnimation();
+    });
+  }
+
+  private applyBounceAnimation() {
+    this.bounceClass = true;
+    // Después de un tiempo, quita la clase de animación
+    setTimeout(() => {
+      this.bounceClass = false;
+    }, 800);
+  }
 }
